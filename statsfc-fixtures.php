8b0c96513429633cfa9b255c7b05428bb298661a
willjw <willjw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-15 16:26:48 +0000

    Fixed broken link in description.
    git-svn-id: https://plugins.svn.wordpress.org/statsfc-fixtures/trunk@726947 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- statsfc-fixtures.php
+++ statsfc-fixtures.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: StatsFC Fixtures
-Plugin URI: https://statsfc.com/developers
+Plugin URI: https://statsfc.com/docs/wordpress
 Description: StatsFC Fixtures
 Version: 1.1.1
 Author: Will Woodward
