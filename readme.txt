01b3434f9e59cfdc76c84169a7d7808922e1d93b
willjw <willjw@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-02 15:47:44 +0000

    Updated error messages.
    git-svn-id: https://plugins.svn.wordpress.org/statsfc-fixtures/trunk@868046 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link:
 Tags: widget, football, soccer, fixtures, premier league, fa cup, league cup
 Requires at least: 3.3
 Tested up to: 3.5.1
-Stable tag: 1.2.5
+Stable tag: 1.2.6
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -86,6 +86,10 @@ If you want to place the widget into a page rather than a sidebar:
 **1.2.5**:
 
 - Fixed possible Timezone bug.
+
+**1.2.6**:
+
+- Tweaked error message.
 
 == Upgrade notice ==
 
--- statsfc-fixtures.php
+++ statsfc-fixtures.php
@@ -3,7 +3,7 @@
 Plugin Name: StatsFC Fixtures
 Plugin URI: https://statsfc.com/docs/wordpress
 Description: StatsFC Fixtures
-Version: 1.2.5
+Version: 1.2.6
 Author: Will Woodward
 Author URI: http://willjw.co.uk
 License: GPL2
@@ -283,7 +283,7 @@ class StatsFC_Fixtures extends WP_Widget {
 			</div>
 		<?php
 		} catch (Exception $e) {
-			echo '<p class="statsfc_error">' . esc_attr($e->getMessage()) .'</p>' . PHP_EOL;
+			echo '<p style="text-align: center;"><img src="//statsfc.com/i/icon.png" width="64" height="64" alt="Football widgets and API"><br><a href="https://statsfc.com" title="Football widgets and API" target="_blank">StatsFC.com</a> – ' . esc_attr($e->getMessage()) .'</p>' . PHP_EOL;
 		}
 
 		echo $after_widget;
